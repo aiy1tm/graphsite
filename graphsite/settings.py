4f1ba541dbef91cad601f52d6d1d831955217125
aiy1tm <sullivan.sts@gmail.com>
2018-12-20 16:02:19 -0800

    update whitenoise static production settings

--- graphsite/settings.py
+++ graphsite/settings.py
@@ -46,6 +46,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,7 +126,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+WHITENOISE_USE_FINDERS = True
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static').replace('\\','/'),]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\','/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\','/')
